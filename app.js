facd4ebd44b3ea41ef7ba9b2e7878f5c1a82568b
Kalpik Mehta <kalpikmehta@gmail.com>
2025-12-17 22:15:51 +0530

    rate limiter

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 const contactRoutes = require('./routes/contactRoutes');
+const rateLimit = require('express-rate-limit');
 
 
 // Connect Database
 connectDB();
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per window (15 mins)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "Too many requests from this IP, please try again after 15 minutes."
+});
+
+app.use(limiter);
 
 // Middleware
 app.use(cors());
